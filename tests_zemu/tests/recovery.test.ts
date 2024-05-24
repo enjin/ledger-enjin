ad8476f65203c6014e4fb1da562e10c6cf86a576
Leonardo Custodio <leonardo@custodio.me>
2024-05-24 13:21:47 -0300

    Fix screenshots

--- tests_zemu/tests/recovery.test.ts
+++ tests_zemu/tests/recovery.test.ts
@@ -15,7 +15,7 @@
  ******************************************************************************* */
 
 import Zemu, { DEFAULT_START_OPTIONS } from '@zondax/zemu'
-import { newPolkadotApp } from '@zondax/ledger-substrate'
+import { newSubstrateApp } from '@zondax/ledger-substrate'
 import { APP_SEED, models } from './common'
 
 const defaultOptions = {
@@ -25,6 +25,8 @@ const defaultOptions = {
   X11: false,
 }
 
+const CHAIN = 'Enjin'
+
 jest.setTimeout(60000)
 
 describe('Recovery', function () {
@@ -32,12 +34,12 @@ describe('Recovery', function () {
     const sim = new Zemu(m.path)
     try {
       await sim.start({ ...defaultOptions, model: m.name })
-      const app = newPolkadotApp(sim.getTransport())
+      const app = newSubstrateApp(sim.getTransport(), CHAIN)
 
+      const enjin_expected_address = 'enEgRU9nN8czrFQputvBDeiimW7HfD2o671kfWcPkk9ni1ukb'
+      const enjin_expected_pk = 'b0eab5dffb19c225c010b61a7f3b6838bb462f70936298a4943711287d2596c9'
       const polkadot_expected_address = '166wVhuQsKFeb7bd1faydHgVvX1bZU2rUuY7FJmWApNz2fQY'
       const polkadot_expected_pk = 'e1b4d72d27b3e91b9b6116555b4ea17138ddc12ca7cdbab30e2e0509bd848419'
-      const kusama_expected_address = '16nK5XEGrPHjSwzHAdkKabmwu6L2t1RGW6drYyGgS84UZDRy'
-      const kusama_expected_pk = 'ffbc10f71d63e0da1b9e7ee2eb4037466551dc32b9d4641aafd73a65970fae42'
 
       let resp = await app.getAddress(0x80000000, 0x80000000, 0x80000000)
 
@@ -46,8 +48,8 @@ describe('Recovery', function () {
       expect(resp.return_code).toEqual(0x9000)
       expect(resp.error_message).toEqual('No errors')
 
-      expect(resp.address).toEqual(polkadot_expected_address)
-      expect(resp.pubKey).toEqual(polkadot_expected_pk)
+      expect(resp.address).toEqual(enjin_expected_address)
+      expect(resp.pubKey).toEqual(enjin_expected_pk)
 
       await sim.enableSpecialMode('Zondax', true)
 
@@ -58,8 +60,8 @@ describe('Recovery', function () {
       expect(resp.return_code).toEqual(0x9000)
       expect(resp.error_message).toEqual('No errors')
 
-      expect(resp.address).toEqual(kusama_expected_address)
-      expect(resp.pubKey).toEqual(kusama_expected_pk)
+      expect(resp.address).toEqual(polkadot_expected_address)
+      expect(resp.pubKey).toEqual(polkadot_expected_pk)
     } finally {
       await sim.close()
     }
